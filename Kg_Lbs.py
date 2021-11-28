9e2de7ee0f7187f837678220e5d47357a13ffd59
Spencer Hurrle <spencer.hurrle@gmail.com>
2021-11-27 22:32:23 -0500

    Modified looping to continue until user quits & added minor improvements

--- Kg_Lbs.py
+++ Kg_Lbs.py
@@ -1,38 +1,33 @@
-print("Hello, This program to convert from Kg-lbs Or Lbs-Kg")
-val1 = input("please select your option \n [1] convert from kg-to-LBS\n [2] for LBS-to-Kg\n [q] to exit \n")
+print("Hello, This program converts from kilograms to pounds or pounds to kilograms!")
+val1 = ''
 
+while val1 != 'q' and val1 != 'Q':
+    val1 = input("Please select your option\n [1] Convert kg-to-LBS\n [2] Convert LBS-to-kg\n [q] to exit \n ")
 
-while True:
-
-  try:
-
-    if val1 == 'q' or val1 == 'Q':
-           print("Thanks you,closing the program...")
-           exit()
-
-    elif val1 == '1':
-             try:
-                   kg = float(input("Kilogram value that you want to convert to LBS: "))
-                   lbs = float(kg * 2.205)
-                   print('The value of {} Kg, is equl to {} lbs'.format(kg, lbs))
-                   break
-             except ValueError:
-                 print("That's not an numeric!")
-
-    elif val1 == '2':
-            try:
-                    lbs = float(input("LBS value that you want to convert to Kg: "))
-                    kg =  float( lbs * 0.4535)
-                    print('The value of %.2f lbs, is equl to %.2f kg' % (lbs, kg))
-                    break
-            except ValueError:
-                    print("That's not an numeric!")
-
+    #try: Commented try block out due to obsolescence. If implemented again, indent as necessary
+    if val1 == '1':  # If user inputs '1', convert kilograms to pounds
+        try:
+            kg = float(input("Value in kgs that you want to convert to lbs: "))
+            lbs = float(kg * 2.205)
+            print('The value of {} Kg, is equal to {} lbs'.format(kg, lbs))
+        except ValueError:
+            print("That's not an numeric!") #Catches when the user enters anything other than a number at conversion
+    elif val1 == '2':  # If user inputs '2', convert pounds to kilograms
+        try:
+            lbs = float(input("Value in lbs that you want to convert to kgs: "))
+            kg = float(lbs * 0.4535)
+            print('The value of %.2f lbs, is equal to %.2f kg' % (lbs, kg))
+        except ValueError:
+            print("That's not an numeric!")
     else:
         print("Unknown value...")
-        exit()
-        break
 
+    print("\nWould you like to continue?\n")
 
-  except ValueError:
-      print("Non.....")
+""" 
+    except ValueError:   
+        print("Non.....")
+"""
+
+print("Thank you, closing the program...") #User has entered 'q', exiting the while loop and stopping the program
+exit()
